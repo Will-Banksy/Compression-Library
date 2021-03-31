f25c7bbb70e9bc40a13963bab302d17c6c58abca
William Banks <williamjb555@gmail.com>
2021-03-31 01:09:02 +0100

    Changed LZOCompression to use stl vectors instead of raw arrays

--- src/lzo/LZOCompression.cpp
+++ src/lzo/LZOCompression.cpp
@@ -2,50 +2,72 @@
 #include "3rdparty/minilzo.h"
 #include <iostream>
 
-Pair<uint8_t*, uint64_t> LZOCompression::Compress(uint8_t* data, uint32_t dataSize) {
+std::vector<uint8_t> LZOCompression::Compress(const std::vector<uint8_t>& data, uint32_t dataSize, bool* error) {
 	if(lzo_init() != LZO_E_OK) {
 		std::cout << "[ERROR]: Initialisation of LZO library failed - This usually indicates a compiler bug" << std::endl;
-		return { nullptr, 0 };
+		if(error) {
+			*error = true;
+		}
+		return std::vector<uint8_t>();
 	}
 
 	uint64_t outputSize = dataSize + dataSize / 16 + 64 + 3;
-	uint8_t* output = new uint8_t[outputSize]; // Make the output block slightly bigger than the input block, in case the data is incompressible
+	std::vector<uint8_t> output; // Make the output block slightly bigger than the input block, in case the data is incompressible
+	output.resize(outputSize);
 
 	uint64_t workingMemorySize = ((LZO1X_1_MEM_COMPRESS) + (sizeof(lzo_align_t) - 1)) / sizeof(lzo_align_t);
 	lzo_align_t* workingMemory = new lzo_align_t[workingMemorySize];
 
-	int result = lzo1x_1_compress(data, dataSize, output, &outputSize, workingMemory);
+	int result = lzo1x_1_compress(&data.front(), dataSize, &output.front(), &outputSize, workingMemory);
 	if(result != LZO_E_OK) {
 		std::cout << "[ERROR]: Internal error - compression failed. Error code: " << result << std::endl;
-		delete[] output;
-		return { nullptr, 0 };
+		if(error) {
+			*error = true;
+		}
+		return std::vector<uint8_t>();
 	}
 
-	return { output, outputSize };
+	output.resize(outputSize);
+
+	if(error) {
+		*error = false;
+	}
+
+	return output;
 }
 
-Pair<uint8_t*, uint32_t> LZOCompression::Decompress(uint8_t* data, uint64_t dataSize, uint32_t decompressedSize) {
+std::vector<uint8_t> LZOCompression::Decompress(const std::vector<uint8_t> data, uint64_t dataSize, uint32_t decompressedSize, bool* error) {
 	if(lzo_init() != LZO_E_OK) {
 		std::cout << "[ERROR]: Initialisation of LZO library failed - This usually indicates a compiler bug" << std::endl;
-		return { nullptr, 0 };
+		if(error) {
+			*error = true;
+		}
+		return std::vector<uint8_t>();
 	}
 
 	uint64_t outputSize = decompressedSize;
-	uint8_t* output = new uint8_t[outputSize];
+	std::vector<uint8_t> output; // Make the output block slightly bigger than the input block, in case the data is incompressible
+	output.resize(outputSize);
 
-	int result = lzo1x_decompress(data, dataSize, output, &outputSize, nullptr);
+	int result = lzo1x_decompress(&data.front(), dataSize, &output.front(), &outputSize, nullptr);
 	if(result != LZO_E_OK) {
 		std::cout << "[ERROR]: Internal error - compression failed. Error code: " << result << std::endl;
-		delete[] output;
-		return { nullptr, 0 };
+		if(error) {
+			*error = true;
+		}
+		return std::vector<uint8_t>();
 	}
 
 	uint32_t outputSizeShort = (outputSize);
 	if(outputSizeShort != outputSize) { // This shouldn't happen, because I think it'd segfault before this
 		std::cout << "[ERROR]: Size of decompressed data is greater than that which a 4 bytes can hold" << std::endl;
-		delete[] output;
-		return { nullptr, 0 };
+		if(error) {
+			*error = true;
+		}
+		return std::vector<uint8_t>();
 	}
 
-	return { output, outputSizeShort };
+	output.resize(outputSizeShort);
+
+	return output;
 }
--- src/lzo/LZOCompression.h
+++ src/lzo/LZOCompression.h
@@ -2,26 +2,18 @@
 #define LZOCOMPRESSION_H
 
 #include <cstdint>
-
-template<typename T1, typename T2>
-struct Pair {
-	T1 first;
-	T2 second;
-
-	Pair(T1 first, T2 second) : first(first), second(second) {}
-};
+#include <vector>
 
 class LZOCompression {
 public:
 	/// Returns a pointer to the compressed data (first) and the size of the compressed data (second)
 	/// Supports compressing up to 0xffffffff bytes, although the output of compression may be bigger
-	/// Note that the returned size of the compressed data won't be equal to the amount of memory allocated for it
-	/// If an error occurs, error message will be sent to stdout and this function will return { nullptr, 0 }
-	static Pair<uint8_t*, uint64_t> Compress(uint8_t* data, uint32_t dataSize);
+	/// If an error occurs, error message will be sent to stdout and this function will return an empty vector, and <code>error</error> will be set to true
+	static std::vector<uint8_t> Compress(const std::vector<uint8_t>& data, uint32_t dataSize, bool* error = nullptr);
 	/// Returns a pointer to the decompressed data (first) and the size of the decompressed data (second)
 	/// Supports decompressing up to 0xffffffffffffffff bytes, although the decompressed data must occupy less than 0xffffffff bytes
-	/// If an error occurs, error message will be sent to stdout and this function will return { nullptr, 0 }
-	static Pair<uint8_t*, uint32_t> Decompress(uint8_t* data, uint64_t dataSize, uint32_t decompressedSize);
+	/// If an error occurs, error message will be sent to stdout and this function will return an empty vector, and <code>error</error> will be set to true
+	static std::vector<uint8_t> Decompress(const std::vector<uint8_t> data, uint64_t dataSize, uint32_t decompressedSize, bool* error = nullptr);
 };
 
 #endif // LZOCOMPRESSION_H
